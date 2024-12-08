418318d77f2392379c198829edb08f65bbc862ff
ELWAZEIR <hossamshapan88@gmail.com>
2024-12-08 10:17:28 +0200

    edit wrong code in redux

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -1,13 +1,11 @@
 import { applyMiddleware, createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import CombineRedusers from "./Reducers/CombineRedusers";
-import { thunk } from "redux-thunk";
+import thunk from "redux-thunk";  // استيراد thunk بشكل صحيح (default import)
 
 const MyStore = createStore(
   CombineRedusers,
-  composeWithDevTools(applyMiddleware(thunk))
+  composeWithDevTools(applyMiddleware(thunk))  // إضافة middleware thunk
 );
-
-//
 
 export default MyStore;
